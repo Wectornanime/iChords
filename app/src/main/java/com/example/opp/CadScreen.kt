999d5a98a7c8faaf2bc43f5efb7b0b38bab26baa
Wectornanime <Wectornanime@gmail.com>
2023-12-02 23:47:21 -0300

    resolve conflicts and add first navegation

---
+++ app/src/main/java/com/example/opp/CadScreen.kt
@@ -0,0 +1,11 @@
+package com.example.opp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cad_screen)
+    }
+}
--- app/src/main/java/com/example/opp/FirstScreen.kt
+++ app/src/main/java/com/example/opp/FirstScreen.kt
@@ -3,19 +3,18 @@ package com.example.opp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.opp.databinding.ActivityMainBinding
+import com.example.opp.databinding.ActivityFirstScreenBinding
 
-class MainActivity : AppCompatActivity() {
+class FirstScreen : AppCompatActivity() {
 
-    private  lateinit var binding: ActivityMainBinding
-
+    private lateinit var binding: ActivityFirstScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityFirstScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.root.setOnClickListener {
-            val navNextScreen = Intent(this, FirstScreen::class.java)
+            val navNextScreen = Intent(this, CadScreen::class.java)
             startActivity(navNextScreen)
         }
     }
