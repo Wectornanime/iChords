d384ab0b194c28018894370c26d4b5180be9dc9f
Wectornanime <Wectornanime@gmail.com>
2023-12-06 13:11:10 -0300

    resolved conflits

--- app/src/main/java/com/example/opp/MainActivity.kt
+++ app/src/main/java/com/example/opp/MainActivity.kt
@@ -2,16 +2,25 @@ package com.example.opp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.replace
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.opp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostController = supportFragmentManager.findFragmentById(R.id.mainFragmentFrame) as NavHostFragment
+        navController = navHostController.navController
+        NavigationUI.setupWithNavController(binding.navBarMain, navController)
     }
 }
