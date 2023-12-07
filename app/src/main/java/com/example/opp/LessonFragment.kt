8c117015391d9423e0b8a2248cbf66624ae2e5ae
Wectornanime <Wectornanime@gmail.com>
2023-12-07 16:34:45 -0300

    add navegation of leasson fragment

--- app/src/main/java/com/example/opp/LessonFragment.kt
+++ app/src/main/java/com/example/opp/LessonFragment.kt
@@ -5,27 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LessonFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LessonFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -34,25 +22,20 @@ class LessonFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lesson, container, false)
+        var view = inflater.inflate(R.layout.fragment_lesson, container, false)
+        view.findViewById<Button>(R.id.frag_leasson_opc0).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_lessonFragment_to_menu_music)
+        }
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LessonFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             LessonFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
