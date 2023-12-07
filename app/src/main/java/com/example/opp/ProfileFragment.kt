7ffc0ecf5a4bbe43e742a42ae54bc34b00d79e73
Wectornanime <Wectornanime@gmail.com>
2023-12-07 19:00:49 -0300

    add settings fragment

--- app/src/main/java/com/example/opp/ProfileFragment.kt
+++ app/src/main/java/com/example/opp/ProfileFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -48,6 +51,10 @@ class ProfileFragment : Fragment() {
         val adapterFriends = MainUserAdapter(requireContext(), itemListFriends)
         recyclerViewFriends.adapter = adapterFriends
         recyclerViewFriends.layoutManager = layoutManagerFriends
+
+        view.findViewById<ImageView>(R.id.imgSett).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_menu_profile_to_settingsFragment)
+        }
 
         return view
 
---
+++ app/src/main/java/com/example/opp/SettingsFragment.kt
@@ -0,0 +1,44 @@
+package com.example.opp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.Navigation
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var view = inflater.inflate(R.layout.fragment_settings, container, false)
+        view.findViewById<ImageView>(R.id.imgBack).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_settingsFragment_to_menu_profile)
+        }
+
+        return view
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SettingsFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
